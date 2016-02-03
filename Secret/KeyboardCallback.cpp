0e0141a4f662f8ca1370b1b532cf811be730f3b6
GM2000 <zgxbsyb@163.com>
2016-02-03 10:01:02 +0800

    修复个小错误

--- Secret/KeyboardCallback.cpp
+++ Secret/KeyboardCallback.cpp
@@ -5,5 +5,5 @@ bool Keys[512]{ false };
 
 void keyboardCallback(GLFWwindow* Window, int Key, int Scancode, int Action, int Mods)
 {
-	Keys[Key] = (!Action == GLFW_RELEASE);
+	Keys[Key] = !(Action == GLFW_RELEASE);
 }
